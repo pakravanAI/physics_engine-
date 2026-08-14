0730409261a6eb65126354896142b503af8f1ff7
Padra_IV <pakravanpadra@gmail.com>
2026-08-14 10:44:58 +0330

    Update engine.py

--- python/engine.py
+++ python/engine.py
@@ -1,4 +1,4 @@
-''' hello i hpe you have a good time with my (or bad i dont care)
+''' hello i hope you have a good time with my (or bad i dont care)
 just do a deep dive in the functions down bellow if yuo want to build you own
 sim with these tools we havw math vecs forces ect ''' 
 from math import sin , cos , atan2 , sqrt
